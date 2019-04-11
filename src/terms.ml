ec461d15457142944e3d53618a0b1fc749250e01
Jeffrey Lin <jeffrey@icurse.nl>
2019-04-11 10:30:44 -0400

    terms.ml: add substitution rules

--- src/terms.ml
+++ src/terms.ml
@@ -30,3 +30,23 @@ let rec bound_vars = function
   | Var _ -> VarSet.empty
   | Abs (x, s) -> VarSet.union (bound_vars s) (VarSet.singleton x)
   | App (s, t) -> VarSet.union (bound_vars s) (bound_vars t)
+
+let alphaset = List.fold_right VarSet.add Misc.alphalist VarSet.empty
+
+let fresh_var set = VarSet.diff alphaset set |> VarSet.choose
+
+(* 2.3.3 *)
+(* s[tt/xx] *)
+let rec substitute tt xx = function
+  | Var x -> if x = xx then tt else Var x
+  | Abs (y, s) ->
+      if xx = y then Abs (y, s) (* λx.s *)
+      else if
+        (not (VarSet.mem xx (free_vars s)))
+        || not (VarSet.mem y (free_vars tt))
+      then Abs (y, substitute tt xx s) (* λy.(s[t/x]) *)
+      else
+        let z = fresh_var (VarSet.union (free_vars s) (free_vars tt)) in
+        Abs (z, s |> substitute (Var z) y |> substitute tt xx)
+        (* λz.(s[z/y][t/x]) *)
+  | App (s, t) -> App (substitute tt xx s, substitute tt xx t)
--- src/terms.mli
+++ src/terms.mli
@@ -11,9 +11,18 @@ val string_of_term : term -> string
 val print_term : term -> unit
 
 module VarSet : sig
+  type elt = string
+
   type t
+
+  val empty : t
+
+  val add : elt -> t -> t
 end
 
 val free_vars : term -> VarSet.t
 
 val bound_vars : term -> VarSet.t
+
+val substitute : term -> string -> term -> term
+(** [substitute tt xx] replaces all instances of xx with tt in a given term. *)
