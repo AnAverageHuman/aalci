2a5101aaa5ebb46bfcbb81a1c87701e403ef7e60
Jeffrey Lin <jeffrey@icurse.nl>
2019-05-05 19:54:14 -0400

    terms: reduce parentheses in string_of_term
    Avoids adding parentheses when there is only a variable left to print.
    
    Inspired by https://ocaml.org/learn/tutorials/format.html

--- src/terms.ml
+++ src/terms.ml
@@ -9,12 +9,14 @@ type term =
 
 let lambda_symb = "λ"
 
-let rec string_of_term =
-  let open Printf in
-  function
-  | Var x -> x
-  | Abs (x, s) -> sprintf "%s%s. %s" lambda_symb x (string_of_term s)
-  | App (s, t) -> sprintf "(%s) (%s)" (string_of_term s) (string_of_term t)
+let rec string_of_term = function
+  | Var s -> s
+  | Abs (x, s) -> Printf.sprintf "%s%s. %s" lambda_symb x (string_of_term s)
+  | App (s, t) -> Printf.sprintf "%s %s" (string_of_exp s) (string_of_exp t)
+
+and string_of_exp = function
+  | Var s -> s
+  | t -> Printf.sprintf "(%s)" (string_of_term t)
 
 let print_term x = x |> string_of_term |> print_endline
 
